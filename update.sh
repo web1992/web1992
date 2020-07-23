2214295b0c6812ef9e4b02d750d1b976e43c4092
web1992 <554933654@qq.com>
2020-07-23 12:09:03 +0800

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+fileDir=/Users/zl/Documents/dev/github/blog/build
 git pull
+
+if [ ! -d "$fileDir" ]; then
+echo "file dir not exist $fileDir"
+exit
+fi
+echo "rm js files"
+
 rm -rf *.js
-cp -rf /Users/zl/Documents/dev/github/blog/build/* .
+cp -rf $fileDir/* .
 git add .
 git commit -m "update"
 git push
